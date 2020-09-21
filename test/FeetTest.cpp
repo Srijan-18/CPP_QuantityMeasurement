91e81421e68ebee4f2f4b224308ecf2c0cfe83bb
Srijan-18 <srijan.mishrasir@gmail.com>
2020-09-21 07:24:17 +0530

    ADDED: Test case for zero feet equality.

--- test/FeetTest.cpp
+++ test/FeetTest.cpp
@@ -1,6 +1,12 @@
 #include "../main/Feet.h"
 #include <gtest/gtest.h>
 
+TEST(FeetTests, _zero_feet_and_zero_feet__should_be_equal)
+{
+    Feet first_value(0), second_value(0);
+    ASSERT_EQ(first_value, second_value);
+}
+
 int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
